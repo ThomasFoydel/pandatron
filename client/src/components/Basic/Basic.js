b8dbd53e7c6ef78c3a03c47ccb0a22de89205d14
ThomasFoydel <thomasjfoydel@gmail.com>
2020-04-06 23:47:00 -0500

    added convolution reverb, wet and dry mix

--- client/src/components/Basic/Basic.js
+++ client/src/components/Basic/Basic.js
@@ -1,21 +1,21 @@
 import React, { useEffect } from 'react';
 import './Basic.scss';
-import block from 'IMreverbs/Block Inside.wav';
+import block from 'IMreverbs/block.irs';
 
 // import example from './example';
 
 const Basic = () => {
   const actx = new AudioContext();
 
-  function getSample(url, cb) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-      actx.decodeAudioData(request.response, cb);
-    };
-    request.send();
-  }
+  // function getSample(url, cb) {
+  //   var request = new XMLHttpRequest();
+  //   request.open('GET', url);
+  //   request.responseType = 'arraybuffer';
+  //   request.onload = function () {
+  //     actx.decodeAudioData(request.response, cb);
+  //   };
+  //   request.send();
+  // }
 
   function makeDistortionCurve(amount) {
     var k = typeof amount === 'number' ? amount : 0,
@@ -31,16 +31,40 @@ const Basic = () => {
     return curve;
   }
 
+  function impulseResponse(duration, decay, reverse) {
+    var sampleRate = actx.sampleRate;
+    var length = sampleRate * duration;
+    var impulse = actx.createBuffer(2, length, sampleRate);
+    var impulseL = impulse.getChannelData(0);
+    var impulseR = impulse.getChannelData(1);
+
+    if (!decay) decay = 2.0;
+    for (var i = 0; i < length; i++) {
+      var n = reverse ? length - i : i;
+      impulseL[i] = (Math.random() * 2 - 1) * Math.pow(1 - n / length, decay);
+      impulseR[i] = (Math.random() * 2 - 1) * Math.pow(1 - n / length, decay);
+    }
+    return impulse;
+  }
+
   const osc1 = actx.createOscillator();
   const osc2 = actx.createOscillator();
   const gain1 = actx.createGain();
   const gain2 = actx.createGain();
+
+  const reverbMixGainWet = actx.createGain();
+  const reverbMixGainDry = actx.createGain();
+  reverbMixGainWet.gain.value = 0.3;
+  reverbMixGainDry.gain.value = 0.7;
+
   const distortion1 = actx.createWaveShaper();
   distortion1.curve = makeDistortionCurve(0);
   const convolver1 = actx.createConvolver();
-  // convolver1.buffer = block;
 
-  console.log(distortion1);
+  var impulseBuffer = impulseResponse(4, 4, false);
+  convolver1.buffer = impulseBuffer;
+  console.log(convolver1);
+
   gain1.gain.value = 0.5;
   gain2.gain.value = 0.5;
 
@@ -49,16 +73,22 @@ const Basic = () => {
   const compressor = actx.createDynamicsCompressor();
 
   osc1.connect(gain1);
+  gain1.connect(oscMasterGain1);
+
   osc2.connect(gain2);
+  gain2.connect(oscMasterGain1);
 
-  gain1.connect(distortion1);
-  gain2.connect(distortion1);
-
-  distortion1.connect(oscMasterGain1);
-
-  oscMasterGain1.connect(filter1);
+  oscMasterGain1.connect(distortion1);
+  distortion1.connect(filter1);
   filter1.connect(compressor);
-  compressor.connect(actx.destination);
+
+  compressor.connect(reverbMixGainDry);
+  compressor.connect(convolver1);
+
+  convolver1.connect(reverbMixGainWet);
+
+  reverbMixGainDry.connect(actx.destination);
+  reverbMixGainWet.connect(actx.destination);
 
   const changeFilter1Type = (e) => {
     filter1.type = e.target.value;
@@ -84,6 +114,27 @@ const Basic = () => {
     gain2.gain.setValueAtTime(e.target.value / 100, actx.currentTime);
   };
 
+  const mixReverbGain = (e) => {
+    console.log('mixReverbGain, e: ', e.target.value);
+    let newDryVal;
+    if (e.target.value < 100) {
+      // newDryVal = 1 - e.target.value / 100;
+      newDryVal = (100 - e.target.value) / 100;
+    } else {
+      newDryVal = 0;
+    }
+
+    console.log('mixReverbGain, newDryVal: ', newDryVal.toFixed(2));
+    reverbMixGainDry.gain.setValueAtTime(
+      newDryVal.toFixed(2),
+      actx.currentTime
+    );
+    reverbMixGainWet.gain.setValueAtTime(
+      e.target.value / 100,
+      actx.currentTime
+    );
+  };
+
   const changeOscMasterGain1 = (e) => {
     oscMasterGain1.gain.linearRampToValueAtTime(
       e.target.value / 100,
@@ -101,6 +152,11 @@ const Basic = () => {
 
   const changeDistortion = (e) => {
     distortion1.curve = makeDistortionCurve(+e.target.value * 5);
+  };
+
+  const changeConvolverReverb = (e) => {
+    const newBuffer = impulseResponse(4, e.target.value, false);
+    convolver1.buffer = newBuffer;
   };
 
   let startedUp = false;
@@ -147,6 +203,15 @@ const Basic = () => {
       </div>
 
       <div className='filter1'>
+        reverb duration
+        <input type='range' onChange={changeConvolverReverb} />
+        mix reverb gain
+        <div>
+          dry<input type='range' onChange={mixReverbGain}></input>wet
+        </div>
+      </div>
+
+      <div className='filter1'>
         filter one
         <select onChange={changeFilter1Type}>
           <option value='lowpass'>lowpass</option>
@@ -177,7 +242,8 @@ const Basic = () => {
           mix gain 1 and 2<input type='range' onChange={mixGain}></input>
         </div>
         <div>
-          master vol<input type='range' onChange={changeOscMasterGain1}></input>
+          osc master gain
+          <input type='range' onChange={changeOscMasterGain1}></input>
         </div>
         <button onClick={startUp}>start up</button>
       </div>
