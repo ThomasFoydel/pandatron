dd38c5d7bfe5314a12291b288436df2332cb6356
ThomasFoydel <thomasjfoydel@gmail.com>
2020-04-28 01:54:52 -0500

    fixed preview picture filename

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Basic from 'components/Basic/Basic';
 import logo from 'imgs/logo/pandatronlogogradient.png';
 import './App.scss';
-import preview from 'imgs/preview/pandatronpreview.jpg';
-console.log('preview url: ', preview);
 
 function App() {
   return (
